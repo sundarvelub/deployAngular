186ceb8b2b0d345e7a72fb79bdd284f6ec4d6069
Sundarvelu Baskaran <sundarvelu@Sundarvelus-MBP.lan>
2020-06-14 20:08:34 +0100

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {HeroesComponent} from './heroes/heroes.component';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hi</h1>
+   {{title}} 
+   <app-heroes></app-heroes>`,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'am here Welcome to the test now';
 }
+ 
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  hero:string = 'Sundarvelu';
+  heroes = ["h1", "h2"];
+
+
+
+}
